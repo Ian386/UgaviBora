2dfdf023936c0503c638f9767f4f9ac725476be7
Ian Ndolo Mwau <134711923+Ian386@users.noreply.github.com>
2024-05-20 09:52:42 +0300

    changes to into

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ nextDom.onclick = function(){
 prevDom.onclick = function(){
     showSlider('prev')
 }
-let timeRunning = 3000;
-let timeAutoNext = 7000
+let timeRunning = 1000;
+let timeAutoNext = 10000
 let runTimeout;
 let runAutoRun = setTimeout(()=>{
     nextDom.click()
